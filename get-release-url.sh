aecb0a385abb2390265711a7babb0cd7954b2da5
Artur H. Lange <artur@arc2.dev>
2024-03-29 20:12:06 +0100

    Initial commit

--- get-release-url.sh
+++ get-release-url.sh
@@ -3,6 +3,24 @@
 #
 # get URL to download latest GTFS feed
 #
+let CURRENT_YEAR=$(date +%Y)
+let NEXT_YEAR=CURRENT_YEAR+1
+RELEASE_URL=https://koleje-ks.pl/gtfs/$CURRENT_YEAR-$NEXT_YEAR.zip
+
+curl -sIfo /dev/null $RELEASE_URL
+
+while [ $? -ne 0 ]
+do
+    if [ $CURRENT_YEAR -le 2000 ]
+    then
+        RELEASE_URL=
+        break
+    fi
+    let CURRENT_YEAR--
+    let NEXT_YEAR--
+    RELEASE_URL=https://koleje-ks.pl/gtfs/$CURRENT_YEAR-$NEXT_YEAR.zip
+    curl -sIfo /dev/null $RELEASE_URL
+done
 
 if [ -n "$RELEASE_URL" ]
 then
